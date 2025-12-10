51f043bf84d1d9e989b110c755f832d5ed43e0ad
Jaclynmcmillan <Jaclyn.m.mcmillan@gmail.com>
2025-12-09 16:14:37 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,19 +5,29 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Solution from './pages/Solution';
+import { usePageTracking } from './hooks/usePageTracking';
+
+const AppRoutes: React.FC = () => {
+  // ✅ Tracks a pageview on every route change
+  usePageTracking();
+
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/solutions/:slug" element={<Solution />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Layout>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/solutions/:slug" element={<Solution />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Layout>
+      <AppRoutes />
     </Router>
   );
 };
